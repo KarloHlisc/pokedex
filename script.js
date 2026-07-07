762853400adffd4edcc8fd1c8de1711ad3058c77
Karlo Hlisc <karlohlisc.kh@gmail.com>
2026-07-07 20:28:41 +0200

    fix: function templatePokemon(pokemonData)

--- script.js
+++ script.js
@@ -47,22 +47,7 @@ async function getPokemonAbilities(pokemonURL) {
 
 function templatePokemon(pokemonsData) {
   let templete = "";
-  templete += `
-    <li>
-    <div class="pokemon-cont">
-        <div class="pokemon-header">
-            <span>#${pokemonsData.id}</span>
-            <h3>${pokemonsData.name}</h3>
-        </div>
-        <div id="poke${pokemonsData.id}" class="pokemon-image-cont ${getBackgroundColorPokemon(pokemonsData)}">
-            <img src="${pokemonsData.sprites.other.dream_world.front_default}" alt="pokemon Bild ${pokemonsData.name}">
-        </div>
-        <div class="types-cont" id=types-cont>
-         ${pokemonTypes(pokemonsData)}
-        </div>
-    </div>
-    </li>
-    `;
+  templete += getTemplatePokemon(pokemonsData);
   pokemonContainer.innerHTML += templete;
 }
 
