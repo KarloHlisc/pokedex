16aea16926816abeca017b83d410f9cb162ab154
Karlo Hlisc <karlohlisc.kh@gmail.com>
2026-07-22 21:41:49 +0200

    fix: search pokemon

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const pokemonContainer = document.getElementById("pokemon");
 let pokemons = [];
 let allPokemonsData = [];
 let indexOffset = 0;
-//let limitPokemon = 20;
+
 const BASE_URL = "https://pokeapi.co/api/v2/";
 
 function init() {
@@ -26,15 +26,13 @@ async function fetchPokemon() {
     return;
   } finally {
   }
- let  countPokemon = indexOffset;
-  for (let index = 0; index <= pokemons.length; index++) {
- //  if(pokemons[index].url === undefined){
-  //  return 0;
- //  }
-   // else{
-      await getPokemonAbilities(pokemons[index].url);
-    //} 
+ countPokemonIndex();
+}
 
+async function countPokemonIndex() {
+  let  countPokemon = indexOffset;
+  for (let index = 0; index < pokemons.length; index++) {
+      await getPokemonAbilities(pokemons[index].url);
     templatePokemon(allPokemonsData[index+countPokemon]);
   }
 }
@@ -71,6 +69,25 @@ function loadMorePokemons() {
   fetchPokemon();
 }
 
+function filterItems(arr, query) {
+ // for(let index=0; index < arr.length; index++){
+  //  let pokemonName = arr[index].name;
+ console.log( arr.includes((el) => el.name.toLowerCase().includes(query.toLowerCase())));
+}
+
+function searchArray() {
+    let btnSearch = document.getElementById('search-btn');
+    let polemonBtnValue = document.getElementById('search-text').value;
+   
+    console.log(filterItems(allPokemonsData,polemonBtnValue));
+    //document.getElementById('pokemon').innerHTML=templatePokemon();
+   showSarchPokemon(polemonBtnValue);
+}
+
+function showSarchPokemon(arr ,el,pokemonsData) {
+    templatePokemon(arr);
+//  document.getElementById('pokemon').innerHTML=templatePokemon(el.name);
+}
 
 
 /*
